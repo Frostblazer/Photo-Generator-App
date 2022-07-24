7bc24406e8f4410a65f8b35a307c6222503089ec
Shawn Witemyre <switemyre@comcast.net>
2022-07-23 23:27:14 -0400

    Added service for api call, added button to trigger the api call, and displayed the photo on the photo-show component

--- src/app/fetch-photo.service.ts
+++ src/app/fetch-photo.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class FetchPhotoService {
   constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            'Client-ID IPovejHHIMWNgruJXdDFeJ3hp1FyTJcD-k4jr9q-564',
+        },
+      }
+    );
+  }
 }
--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchPhotoService } from '../fetch-photo.service';
 
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
-  styleUrls: ['./photo-show.component.css']
+  styleUrls: ['./photo-show.component.css'],
 })
 export class PhotoShowComponent implements OnInit {
+  photoUrl: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fetchPhotoService: FetchPhotoService) {
+    this.fetchPhoto();
   }
 
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
+    this.fetchPhotoService.getPhoto().subscribe((response) => {
+      this.photoUrl = response.urls.regular;
+    });
+  }
+
+  ngOnInit(): void {}
 }
